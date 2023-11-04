5721bed79590fe78e88218c8cfb0d01486ae4a63
Wyatt VO <votranquy96@gmail.com>
2023-11-04 21:02:42 +0700

    Add CarSerializer

---
+++ car/serializers.py
@@ -0,0 +1,19 @@
+#car/serializers.py
+from rest_framework import serializers
+from car.models import Car
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ('name', 'color', 'brand')
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['email', 'password']
+#         extra_kwargs = {'password': {'write_only': True}}
+
+
+# class UserLoginSerializer(serializers.Serializer):
+#     email = serializers.EmailField(required=True)
+#     password = serializers.CharField(required=True)
