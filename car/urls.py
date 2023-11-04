e7302e056dbf7f97b07cfc85e0484a8401ec98c2
Wyatt VO <votranquy96@gmail.com>
2023-11-04 21:07:49 +0700

    Add url to use view

---
+++ car/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('cars', views.ListCreateCarView.as_view()),
+    path('cars/<int:pk>', views.UpdateDeleteCarView.as_view()),
+]
